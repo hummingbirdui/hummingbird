b699fd6f010ae35c2e7c02ede9272e1d16650b51
Khayrul Islam <khayrulislam2525@gmail.com>
2025-05-16 21:32:06 +0600

    Add type declaration for bootstrap imports

---
+++ bootstrap.d.ts
@@ -0,0 +1,4 @@
+declare module "bootstrap/js/dist/util/index.js";
+declare module "bootstrap/js/dist/dom/data";
+declare module "bootstrap/js/dist/dom/event-handler.js";
+declare module "bootstrap/js/dist/dom/selector-engine.js";
