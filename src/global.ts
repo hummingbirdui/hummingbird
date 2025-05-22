bf26f3d756564ce4bc0df2db6e9728a16d06cb9c
Khayrul Islam <khayrulislam2525@gmail.com>
2025-05-22 09:24:36 +0600

    feat: Add Tooltip component

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,9 @@
 import Dropdown from './scripts/dropdown';
+import Tooltip from './scripts/tooltip';
 
 declare global {
   interface Window {
     Dropdown: typeof Dropdown;
+    Tooltip: typeof Tooltip;
   }
 }
