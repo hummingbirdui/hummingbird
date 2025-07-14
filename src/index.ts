ca5612f8d9402e6a6cf74307303b9922d7d20600
Khayrul Islam <khayrulislam2525@gmail.com>
2025-07-14 16:30:20 +0600

    refactor: Sort component

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,53 @@
+export { default as Alert, type AlertInstance, type AlertStatic, type AlertEvents } from './scripts/alert';
+
 export { default as Button, type ButtonInstance, type ButtonStatic } from './scripts/button';
-export {
-  default as Dropdown,
-  type DropdownOptions,
-  type DropdownInstance,
-  type DropdownStatic,
-} from './scripts/dropdown';
-
-export { default as Tooltip, type TooltipOptions, type TooltipInstance, type TooltipStatic } from './scripts/tooltip';
-
-export { default as Alert, type AlertInstance, type AlertStatic } from './scripts/alert';
-
-export {
-  default as Collapse,
-  type CollapseOptions,
-  type CollapseInstance,
-  type CollapseStatic,
-} from './scripts/collapse';
-
-export {
-  default as Offcanvas,
-  type OffcanvasOptions,
-  type OffcanvasInstance,
-  type OffcanvasStatic,
-} from './scripts/offcanvas';
-
-export { default as Modal, type ModalOptions, type ModalInstance, type ModalStatic } from './scripts/modal';
 
 export {
   default as Carousel,
   type CarouselOptions,
   type CarouselInstance,
   type CarouselStatic,
+  type CarouselEvents,
 } from './scripts/carousel';
 
-export { default as Tab, type TabInstance, type TabStatic } from './scripts/tab';
+export {
+  default as Collapse,
+  type CollapseOptions,
+  type CollapseInstance,
+  type CollapseStatic,
+  type CollapseEvents,
+} from './scripts/collapse';
+
+export {
+  default as Dropdown,
+  type DropdownOptions,
+  type DropdownInstance,
+  type DropdownStatic,
+  type DropdownEvents,
+} from './scripts/dropdown';
+
+export {
+  default as Modal,
+  type ModalOptions,
+  type ModalInstance,
+  type ModalStatic,
+  type ModalEvents,
+} from './scripts/modal';
+
+export {
+  default as Offcanvas,
+  type OffcanvasOptions,
+  type OffcanvasInstance,
+  type OffcanvasStatic,
+  type OffcanvasEvents,
+} from './scripts/offcanvas';
+
+export { default as Tab, type TabInstance, type TabStatic, type TabEvents } from './scripts/tab';
+
+export {
+  default as Tooltip,
+  type TooltipOptions,
+  type TooltipInstance,
+  type TooltipStatic,
+  type TooltipEvents,
+} from './scripts/tooltip';
