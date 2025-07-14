ddee990b4913ced8de48fb48e8055fcb74282312
Khayrul Islam <khayrulislam2525@gmail.com>
2025-07-14 16:37:12 +0600

    remove extra bootstrap.d.ts file

--- bootstrap.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "bootstrap/js/dist/util/index.js";
-declare module "bootstrap/js/dist/dom/data";
-declare module "bootstrap/js/dist/dom/event-handler.js";
-declare module "bootstrap/js/dist/dom/selector-engine.js";
