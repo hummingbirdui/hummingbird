15d49b920a6121d6e34dff89bbc23fc1234f3f5b
Khayrul Islam <khayrulislam2525@gmail.com>
2025-07-14 16:30:42 +0600

    removed unused global and utils file

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const docReady = (fn: () => void) => {
-  if (document.readyState === "loading") {
-    window.addEventListener("DOMContentLoaded", fn);
-  } else {
-    setTimeout(fn, 1);
-  }
-};
