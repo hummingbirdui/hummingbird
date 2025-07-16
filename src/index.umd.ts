28174f09464dfce30898927c0a042106ecce27b7
Khayrul Islam <khayrulislam2525@gmail.com>
2025-07-16 09:26:55 +0600

    Fix: Remove UMD extra code

--- src/index.umd.ts
+++ src/index.umd.ts
@@ -1,29 +1,21 @@
-import * as Hummingbird from './index';
+import Alert from './scripts/alert';
+import Button from './scripts/button';
+import Carousel from './scripts/carousel';
+import Collapse from './scripts/collapse';
+import Dropdown from './scripts/dropdown';
+import Modal from './scripts/modal';
+import Offcanvas from './scripts/offcanvas';
+import Tab from './scripts/tab';
+import Tooltip from './scripts/tooltip';
 
-declare global {
-  interface Window {
-    Alert: typeof Hummingbird.Alert;
-    Button: typeof Hummingbird.Button;
-    Carousel: typeof Hummingbird.Carousel;
-    Collapse: typeof Hummingbird.Collapse;
-    Dropdown: typeof Hummingbird.Dropdown;
-    Modal: typeof Hummingbird.Modal;
-    Offcanvas: typeof Hummingbird.Offcanvas;
-    Tab: typeof Hummingbird.Tab;
-    Tooltip: typeof Hummingbird.Tooltip;
-  }
-}
-
-if (typeof window !== 'undefined') {
-  window.Alert = Hummingbird.Alert;
-  window.Button = Hummingbird.Button;
-  window.Carousel = Hummingbird.Carousel;
-  window.Collapse = Hummingbird.Collapse;
-  window.Dropdown = Hummingbird.Dropdown;
-  window.Modal = Hummingbird.Modal;
-  window.Offcanvas = Hummingbird.Offcanvas;
-  window.Tab = Hummingbird.Tab;
-  window.Tooltip = Hummingbird.Tooltip;
-}
-
-export * from './index';
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Tab,
+  Tooltip,
+};
