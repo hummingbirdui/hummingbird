458ad50c01422045d29ec58185add2f119985ccd
Khayrul Islam <khayrulislam2525@gmail.com>
2025-08-26 12:21:24 +0600

    Updates

--- apps/const.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = 'Hummingbird';
-export const SITE_DESCRIPTION =
-  'An open-source system designed for rapid development, without sacrificing the granular control of utility-first CSS.';
