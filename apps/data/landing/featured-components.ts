f12a16583c5b33acd69cd0a6d3b1690c87a105f5
riazul islam <riazul5207@gmail.com>
2025-08-28 10:49:24 +0600

    refactored

--- apps/data/landing/featured-components.ts
+++ apps/data/landing/featured-components.ts
@@ -8,21 +8,31 @@ import breadcrumb from '@assets/images/landing/components/light/breadcrumb.png';
 import card from '@assets/images/landing/components/light/card.png';
 import chip from '@assets/images/landing/components/light/chip.png';
 
+import accordionDark from '@assets/images/landing/components/dark/accordian.png';
+import avatarDark from '@assets/images/landing/components/dark/avatar.png';
+import alertDark from '@assets/images/landing/components/dark/alert.png';
+import badgeDark from '@assets/images/landing/components/dark/badge.png';
+import buttonDark from '@assets/images/landing/components/dark/button.png';
+import buttonGroupDark from '@assets/images/landing/components/dark/button-group.png';
+import breadcrumbDark from '@assets/images/landing/components/dark/breadcrumb.png';
+import cardDark from '@assets/images/landing/components/dark/card.png';
+import chipDark from '@assets/images/landing/components/dark/chip.png';
+
 export interface Component {
-  image: ImageMetadata;
+  image: { light: ImageMetadata; dark: ImageMetadata };
   name: string;
   count: number;
   url: string;
 }
 
 export const featuredComponents: Component[] = [
-  { image: accordion, name: 'Accordion', count: 2, url: '#' },
-  { image: alert, name: 'Alert', count: 3, url: '#' },
-  { image: avatar, name: 'Avatar', count: 14, url: '#' },
-  { image: badge, name: 'Badge', count: 14, url: '#' },
-  { image: breadcrumb, name: 'Breadcrumb', count: 2, url: '#' },
-  { image: button, name: 'Button', count: 20, url: '#' },
-  { image: buttonGroup, name: 'Button Group', count: 2, url: '#' },
-  { image: card, name: 'Card', count: 20, url: '#' },
-  { image: chip, name: 'Chip', count: 20, url: '#' },
+  { image: { light: accordion, dark: accordionDark }, name: 'Accordion', count: 2, url: '#' },
+  { image: { light: alert, dark: alertDark }, name: 'Alert', count: 3, url: '#' },
+  { image: { light: avatar, dark: avatarDark }, name: 'Avatar', count: 14, url: '#' },
+  { image: { light: badge, dark: badgeDark }, name: 'Badge', count: 14, url: '#' },
+  { image: { light: breadcrumb, dark: breadcrumbDark }, name: 'Breadcrumb', count: 2, url: '#' },
+  { image: { light: button, dark: buttonDark }, name: 'Button', count: 20, url: '#' },
+  { image: { light: buttonGroup, dark: buttonGroupDark }, name: 'Button Group', count: 2, url: '#' },
+  { image: { light: card, dark: cardDark }, name: 'Card', count: 20, url: '#' },
+  { image: { light: chip, dark: chipDark }, name: 'Chip', count: 20, url: '#' },
 ];
