73c3027613e019d0a1482333b81b025e4554b1ff
Khayrul Islam <khayrulislam2525@gmail.com>
2025-09-21 14:55:23 +0600

    docs: update components

--- apps/docs.ts
+++ apps/docs.ts
@@ -27,9 +27,9 @@ const toggleTheme = (theme: Theme) => {
   requestAnimationFrame(() => requestAnimationFrame(() => s.remove()));
 };
 
-const updateToggleThemeButton = (theme: Theme) => {
-  const moonIcon = document.querySelector('[data-theme-toggle="dark"]');
-  const sunIcon = document.querySelector('[data-theme-toggle="light"]');
+const updateToggleThemeButton = (theme: Theme, toggleThemeButton?: Element) => {
+  const moonIcon = (toggleThemeButton || document).querySelector('[data-theme-toggle="dark"]');
+  const sunIcon = (toggleThemeButton || document).querySelector('[data-theme-toggle="light"]');
   if (theme === 'dark') {
     moonIcon?.classList.add('hidden');
     sunIcon?.classList.remove('hidden');
@@ -45,13 +45,15 @@ document.addEventListener('DOMContentLoaded', () => {
     savedTheme ?? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
   toggleTheme(initialTheme);
   updateToggleThemeButton(initialTheme);
-  const toggleThemeBtn = document.querySelector('[data-theme-toggle-btn]');
-  toggleThemeBtn?.addEventListener('click', () => {
-    const current = (localStorage.getItem('theme') as Theme | null) ?? initialTheme;
-    const newTheme: Theme = current === 'dark' ? 'light' : 'dark';
-    localStorage.setItem('theme', newTheme);
-    toggleTheme(newTheme);
-    updateToggleThemeButton(newTheme);
+  const toggleThemeBtns = document.querySelectorAll('[data-theme-toggle-btn]');
+  toggleThemeBtns.forEach((button) => {
+    button?.addEventListener('click', () => {
+      const current = (localStorage.getItem('theme') as Theme | null) ?? initialTheme;
+      const newTheme: Theme = current === 'dark' ? 'light' : 'dark';
+      localStorage.setItem('theme', newTheme);
+      toggleTheme(newTheme);
+      updateToggleThemeButton(newTheme, button);
+    });
   });
 
   // preventing browser default scroll to top behavior for a tag
--- apps/sitemap.ts
+++ apps/sitemap.ts
@@ -12,15 +12,19 @@ const sitemap: Sitemap[] = [
     items: [
       {
         name: 'Introduction',
-        path: '#',
+        path: '/docs/getting-started/introduction',
       },
       {
         name: 'Installation',
         path: '/docs/getting-started/installation',
       },
       {
-        name: 'Tutorial',
-        path: '#',
+        name: 'JavaScript',
+        path: '/docs/getting-started/javascript',
+      },
+      {
+        name: 'Accessibility',
+        path: '/docs/getting-started/accessibility',
       },
     ],
   },
@@ -86,8 +90,20 @@ const sitemap: Sitemap[] = [
     subHeader: 'Layout',
     items: [
       {
-        name: 'Grid',
-        path: '/docs/layout/grid',
+        name: 'Container',
+        path: '/docs/layout/container',
+      },
+      {
+        name: 'Bootstrap Grid',
+        path: '/docs/layout/bootstrap-grid',
+      },
+      {
+        name: 'Columns',
+        path: '/docs/layout/columns',
+      },
+      {
+        name: 'Gutters',
+        path: '/docs/layout/gutters',
       },
     ],
   },
