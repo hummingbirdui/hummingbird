6ddfaa5437921a7a756d2ef1faabba7d79012e73
Khayrul Islam <khayrulislam2525@gmail.com>
2025-11-20 09:08:24 +0600

    docs: update github star script

--- apps/libs/github.ts
+++ apps/libs/github.ts
@@ -1,12 +1,20 @@
+let etag: string | null = null;
 let cache: any = null;
-let lastFetch = 0;
 
 export async function getRepoData() {
-  const now = Date.now();
-  if (!cache || now - lastFetch > 60 * 60 * 1000) {
-    const res = await fetch("https://api.github.com/repos/hummingbirdui/hummingbird");
-    cache = await res.json();
-    lastFetch = now;
+  const headers: any = {};
+
+  if (etag) headers['If-None-Match'] = etag;
+
+  const res = await fetch('https://api.github.com/repos/hummingbirdui/hummingbird', {
+    headers,
+  });
+
+  if (res.status === 304) {
+    return cache;
   }
+
+  etag = res.headers.get('ETag');
+  cache = await res.json();
   return cache;
 }
